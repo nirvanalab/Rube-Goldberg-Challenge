d6b340f5ffb622344636b22a5e255f6a15078af1
Vidhur Voora <lab.nirvana@gmail.com>
2017-12-16 23:43:14 -0800

    Added Fan to the menu
    Made the trampoline bouncy

---
+++ Assets/_Project/Scripts/FanSpeed.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FanSpeed : MonoBehaviour {
+
+    public int fanSpeed = 30;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private void onTriggerEnter(Collider col)
+    {
+        if (col.gameObject.CompareTag("Throwable"))
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.forward * fanSpeed, ForceMode.Acceleration);
+        }
+    }
+}
